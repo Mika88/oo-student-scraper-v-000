06869028eebdf3d7020cdee348cffca34dc57cb5
Mika88 <mikaspivak@gmail.com>
2018-09-01 18:37:57 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,9 +20,9 @@ class Student
 
   def add_student_attributes(attributes_hash)
       attributes_hash.each do |k,v|
-        self.send "#{k}=", v 
+        self.send "#{k}=", v
       end
-      self     
+      self
   end
 
   def self.all
